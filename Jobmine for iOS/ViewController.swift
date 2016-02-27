13859d7ce03de6d2047837a34287e6dc85168d98
Uzair Haq <uhaq95@gmail.com>
2016-02-26 20:13:57 -0500

    Initial Commit

---
+++ Jobmine for iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jobmine for iOS
+//
+//  Created by Uzair Haq on 2016-02-26.
+//  Copyright © 2016 Uzair Haq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
